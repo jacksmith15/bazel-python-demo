ea57387987b7e8c718e399704fc9e3b74c25fd40
Radek Lát <radek-lat@cookpad.com>
2022-10-11 16:35:05 +0100

    Add branch coverage support

--- coverage.sh
+++ coverage.sh
@@ -10,7 +10,7 @@ COVERAGE_REPORT_DIR="reports/coverage"
 bazel coverage //...
 
 # Convert combined LCOV file to HTML (depends on lcov being installed):
-bazel run -- @lcov//:genhtml -o $COVERAGE_REPORT_DIR bazel-out/_coverage/_coverage_report.dat
+bazel run -- @lcov//:genhtml --rc lcov_branch_coverage=1 -o $COVERAGE_REPORT_DIR bazel-out/_coverage/_coverage_report.dat
 
 # Open the coverage report:
 xdg-open "${COVERAGE_REPORT_DIR}/index.html"
