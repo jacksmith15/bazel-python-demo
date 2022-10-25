ff05493ecf858fadb771f34ed98de11cf1d16fb2
Jack Smith <jack.je.smith@gmail.com>
2022-10-25 16:09:22 +0100

    Single arg

--- tools/inspection/build-graph/diff.py
+++ tools/inspection/build-graph/diff.py
@@ -13,7 +13,7 @@ def main():
     parser = argparse.ArgumentParser(description="Generate a diff from Bazel build graphs.")
     parser.add_argument("-o", "--output", choices=["package", "target"], default="target")
     parser.add_argument("-s", "--strategy", choices=["checkout", "commit"], default="checkout")
-    parser.add_argument("source_commit", nargs=1)
+    parser.add_argument("source_commit")
     parser.add_argument("target_commit", nargs="?")
     args = parser.parse_args()
     assert args.strategy != "commit", "--strategy=commit not yet supported"
